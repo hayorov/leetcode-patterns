c6cf881687b16b02af8f749aa1bb1f6918cc99c2
Alex Khaerov <hayorov@users.noreply.github.com>
2021-04-21 12:23:01 +0800

    First shoot

---
+++ 01-contains-dublicate.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/contains-duplicate/
+
+
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return not len(set(nums)) == len(nums)
