0f3c922e4d6d817ba156b36921f417f5837949d6
Alex Khaerov <i@hayorov.ru>
2021-06-21 15:18:37 +0800

    Add solution

---
+++ 12-two-sum.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        acc = {}
+        for pos, value in enumerate(nums):
+            if target - value in acc:
+                return [acc[target - value], pos]
+            acc[value] = pos
