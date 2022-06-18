01765d1f620b9ebedc00c47d179b7a2e1578c7ff
Alex Khaerov <hayorov@users.noreply.github.com>
2022-06-18 21:08:48 +0800

    Update 01-contains-dublicate.py

--- 01-contains-dublicate.py
+++ 01-contains-dublicate.py
@@ -7,3 +7,18 @@ from typing import List
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return not len(set(nums)) == len(nums)
+
+
+# less elegant with any
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        inv = {}
+        for num in nums:
+            if not num in inv:
+                inv.update({num: False})
+            else:
+                inv.update({num: True})
+        if any(inv.values()):
+            return True
+        if not any(inv.values()):
+            return False
