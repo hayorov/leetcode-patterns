da888695e5766a7d338448cf2559f78311d92ae3
Alex Khaerov <i@hayorov.ru>
2022-06-21 22:00:05 +0800

    Group anagram

---
+++ 004-group-anagram.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/group-anagrams/
+
+import collections
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
