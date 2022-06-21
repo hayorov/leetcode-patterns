b7ae95acedfa11af2ec77fd865f308d4e71af880
Alex Khaerov <i@hayorov.ru>
2022-06-21 20:57:46 +0800

    Add less elegant

--- 12-two-sum.py
+++ 12-two-sum.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/two-sum/
 
 from typing import List
+from collections import Counter
 
 
 class Solution:
@@ -10,3 +11,17 @@ class Solution:
             if target - value in acc:
                 return [acc[target - value], pos]
             acc[value] = pos
+
+
+# less elegant
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        inv = Counter(nums)
+        for pos_f, num in enumerate(nums):
+            candid = target - num
+            if candid in inv:
+                if candid != num or inv[candid] > 1:
+                    break
+        for pos_s, num in enumerate(nums):
+            if pos_s != pos_f and num == candid:
+                return (pos_f, pos_s)
