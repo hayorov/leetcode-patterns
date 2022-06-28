261ff1b697817190adbda6c5ae8eb034e061dc57
Alex Khaerov <i@hayorov.ru>
2022-06-29 00:03:42 +0800

    valid-palindrome

---
+++ 010-valid-palindrome.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/valid-palindrome/
+
+from string import ascii_lowercase, digits
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st = "".join(
+            i.lower() if i.lower() in ascii_lowercase + digits else "" for i in s
+        )
+        l, r = 0, len(st) - 1
+        while l < r:
+            if st[l] != st[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
